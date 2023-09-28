0eb0947b893dafa68ff050ac79ae6fcdcc1cdcbd
benaldes <benalds12@gmail.com>
2023-09-29 00:18:57 +0300

    Analytics

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,18 @@
+using System;
+using Unity.Services.Core;
+using UnityEngine;
+
+public class Analytics : MonoBehaviour
+{
+    async void Awake()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+}
