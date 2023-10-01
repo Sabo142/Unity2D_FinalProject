833952f02be067febcef7cfb3460074028995f80
ThePotatoElite <alon.landau99@gmail.com>
2023-10-01 21:15:25 +0300

    Added drag options, indicators
    Need a little fix

--- Assets/Scripts/DragItem.cs
+++ Assets/Scripts/DragItem.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class DragItem : MonoBehaviour, IDragHandler
+public class DragItem : MonoBehaviour, IDragHandler/*, IBeginDragHandler, IEndDragHandler */
 {
+    [SerializeField] private GameObject highlightObject;
+    [SerializeField] private Image itemIconImage;
+    public void OnDragBegin(PointerEventData eventData)
+    {
+        itemIconImage.color = new Color(itemIconImage.color.r, itemIconImage.color.g, itemIconImage.color.b, 0.5f);
+        highlightObject.SetActive(true);
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
+        // transform.position = (Vector3)eventData.position;
+    }
+    public void OnDragEnd(PointerEventData eventData) 
+    {
+        itemIconImage.color = new Color(itemIconImage.color.r, itemIconImage.color.g, itemIconImage.color.b, 1f);
+        highlightObject.SetActive(false);
     }
 }
