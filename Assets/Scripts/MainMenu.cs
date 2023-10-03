4dba3f0c13260767aee2df5d5e92aa4038f55571
sabo142 <gabikrivonos@gmail.com>
2023-10-03 11:35:08 +0300

    u

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,11 +4,18 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private HighScore highScore;
 
+    void start()
+    {
+        AnalyticsService.Instance.StartDataCollection();
+    }
     public void OnNameTextChange(string input)
     {
         highScore.PlayerName = input;
